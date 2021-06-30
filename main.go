af7e7599659c08a2d9f048a7ccbb65ea5e1a0f9b
unknown <jjkim0816@gmail.com>
2021-06-30 22:54:28 +0900

    feat:작업중

--- main.go
+++ main.go
@@ -5,7 +5,9 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
 	"strings"
+	"time"
 
 	"golang.org/x/text/encoding/korean"
 	"golang.org/x/text/transform"
@@ -16,24 +18,52 @@ var path []string
 // 성경
 type BibleBook struct {
 	Title  string
-	Phrase []BiblePhrase
+	Phrase []BibleChapter
 }
 
 // 구
-type BiblePhrase struct {
+type BibleChapter struct {
 	Num    int
-	Clause []BibleClause
+	Clause []BibleVerse
 }
 
 // 절
-type BibleClause struct {
+type BibleVerse struct {
 	Num     int
 	Content string
 }
 
+// method
+// bible
+func (bb *BibleBook) SetTitle(data []byte) {
+	bb.Title = string(data)
+}
+
+// chapter
+func (bc *BibleChapter) SetChpaterNum(data []byte) {
+	bc.Num, _ = strconv.Atoi(string(data))
+}
+
+func (bc *BibleChapter) SetChapter(data BibleVerse) {
+	bc.Clause = append(bc.Clause, data)
+}
+
+// verse
+func (bv *BibleVerse) SetVerseNum(data []byte) {
+	bv.Num, _ = strconv.Atoi(string(data))
+}
+
+func (bv *BibleVerse) SetVerse(data []byte) {
+	bv.Content = string(data)
+}
+
 func main() {
 	fmt.Println("start bible parsor")
-	bible := BibleBook{}
+
+	book := BibleBook{}
+	// chapter := BibleChapter{}
+	// verse := BibleVerse{}
+	first := false
 
 	root, _ := os.Getwd()
 	path = append(path, root)
@@ -49,6 +79,8 @@ func main() {
 
 	reader := bufio.NewReader(open)
 	for {
+		time.Sleep(time.Second * time.Duration(1))
+
 		line, isPrefix, err := reader.ReadLine()
 		if isPrefix || err != nil {
 			log.Fatal("isPrefix : ", isPrefix, ", err : ", err)
@@ -65,7 +97,25 @@ func main() {
 		fmt.Printf("text [%d]: %s\n", n, string(text))
 		fmt.Printf("text [%d]: %v\n", n, text)
 
-		// 첫번째 줄은 제목
+		// 값이 없는 경우 continue
+		if len(text) == 0 {
+			fmt.Println("len(text): '", len(text), "' is skip...")
+			continue
+		}
 
+		// 첫번째 줄은 제목
+		if !first {
+			book.SetTitle(text)
+			first = true
+			continue
+		}
+
+		// 절이 아닌 경우 continue
+		if text[0] < 48 || text[0] > 57 {
+			fmt.Println("text[0] : '", text[0], "' is skip...")
+			continue
+		}
+
+		//
 	}
 }
